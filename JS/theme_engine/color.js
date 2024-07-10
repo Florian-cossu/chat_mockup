8d57db31c3ae90e5d3b29f2f2e737249056f7d1c
FlorianCossu <florian.cossu@gmail.com>
2024-07-11 00:08:30 +0200

    Theme engine update/optimisation Modified themeColor constant that is fed into the theme engine for improved code legibility and maintainability. Closes #5 Thanks @leohubert for the tip !

--- JS/theme_engine/color.js
+++ JS/theme_engine/color.js
@@ -10,128 +10,124 @@ function getRandomColor() {
 }
 
 // Declaration of constant that contains the different set of colors for the chat mockup GUI
-const colorThemes = {
-	1: [
-		"#489ed9",
-		"#62b7d9",
-		"linear-gradient(150deg, rgba(87, 113, 254, 1) 0%, rgba(35, 158, 231, 1) 50%, rgba(137, 211, 196, 1) 100%)"
-	],
-	2: [
-		"#eb64ff",
-		"#f08eff",
-		"linear-gradient(150deg, rgba(122, 8, 141, 1) 0%, rgba(203, 50, 172, 1) 50%, rgba(253, 81, 145, 1) 100%)"
-	],
-	3: [
-		"#59d948",
-		"#81e474",
-		"linear-gradient(150deg, rgba(44, 141, 8, 1) 0%, rgba(92, 203, 50, 1) 50%, rgba(139, 253, 81, 1) 100%)"
-	],
-	4: [
-		"#ff9a8b",
-		"#ff6a88",
-		"linear-gradient(150deg, rgba(255, 154, 139, 1) 0%, rgba(255, 106, 136, 1) 50%, rgba(255, 63, 63, 1) 100%)"
-	],
-	5: [
-		"#8e2de2",
-		"#4a00e0",
-		"linear-gradient(150deg, rgba(142, 45, 226, 1) 0%, rgba(74, 0, 224, 1) 50%, rgba(0, 0, 128, 1) 100%)"
-	],
-	6: [
-		"#f7971e",
-		"#ffd200",
-		"linear-gradient(150deg, rgba(247, 151, 30, 1) 0%, rgba(255, 210, 0, 1) 50%, rgba(255, 170, 0, 1) 100%)"
-	],
-	7: [
-		"#00c6ff",
-		"#0072ff",
-		"linear-gradient(150deg, rgba(0, 198, 255, 1) 0%, rgba(0, 114, 255, 1) 50%, rgba(0, 50, 255, 1) 100%)"
-	],
-	8: [
-		"#76b852",
-		"#8dc26f",
-		"linear-gradient(150deg, rgba(118, 184, 82, 1) 0%, rgba(141, 194, 111, 1) 50%, rgba(155, 210, 120, 1) 100%)"
-	],
-	9: [
-		"#fc4a1a",
-		"#f7b733",
-		"linear-gradient(150deg, rgba(252, 74, 26, 1) 0%, rgba(247, 183, 51, 1) 50%, rgba(251, 114, 76, 1) 100%)"
-	],
-	10: [
-		"#11998e",
-		"#38ef7d",
-		"linear-gradient(150deg, rgba(17, 153, 142, 1) 0%, rgba(56, 239, 125, 1) 50%, rgba(43, 212, 100, 1) 100%)"
-	],
-	11: [
-		"#ee0979",
-		"#ff6a00",
-		"linear-gradient(150deg, rgba(238, 9, 121, 1) 0%, rgba(255, 106, 0, 1) 50%, rgba(255, 40, 0, 1) 100%)"
-	],
-	12: [
-		"#283c86",
-		"#45a247",
-		"linear-gradient(150deg, rgba(40, 60, 134, 1) 0%, rgba(69, 162, 71, 1) 50%, rgba(56, 168, 50, 1) 100%)"
-	],
-	13: [
-		"#1f4037",
-		"#99f2c8",
-		"linear-gradient(150deg, rgba(31, 64, 55, 1) 0%, rgba(153, 242, 200, 1) 50%, rgba(0, 184, 148, 1) 100%)"
-	],
-	14: [
-		"#c33764",
-		"#1d2671",
-		"linear-gradient(150deg, rgba(195, 55, 100, 1) 0%, rgba(29, 38, 113, 1) 50%, rgba(0, 0, 50, 1) 100%)"
-	],
-	15: [
-		"#ff7e5f",
-		"#feb47b",
-		"linear-gradient(150deg, rgba(255, 126, 95, 1) 0%, rgba(254, 180, 123, 1) 50%, rgba(255, 140, 0, 1) 100%)"
-	],
-	16: [
-		"#6a11cb",
-		"#2575fc",
-		"linear-gradient(150deg, rgba(106, 17, 203, 1) 0%, rgba(37, 117, 252, 1) 50%, rgba(0, 64, 255, 1) 100%)"
-	],
-	17: [
-		"#e53935",
-		"#e35d5b",
-		"linear-gradient(150deg, rgba(229, 57, 53, 1) 0%, rgba(227, 93, 91, 1) 50%, rgba(255, 0, 0, 1) 100%)"
-	],
-	18: [
-		"#56ab2f",
-		"#a8e063",
-		"linear-gradient(150deg, rgba(86, 171, 47, 1) 0%, rgba(168, 224, 99, 1) 50%, rgba(124, 252, 0, 1) 100%)"
-	],
-	19: [
-		"#e44d26",
-		"#f16529",
-		"linear-gradient(150deg, rgba(228, 77, 38, 1) 0%, rgba(241, 101, 41, 1) 50%, rgba(255, 165, 0, 1) 100%)"
-	],
-	20: [
-		"#34e89e",
-		"#0f3443",
-		"linear-gradient(150deg, rgba(52, 232, 158, 1) 0%, rgba(15, 52, 67, 1) 50%, rgba(0, 128, 128, 1) 100%)"
-	]
-};
+const colorThemes = [
+	{
+		accent_color: "#489ed9",
+		accent_color_interaction: "#62b7d9",
+		gradient: "linear-gradient(150deg, rgba(87, 113, 254, 1) 0%, rgba(35, 158, 231, 1) 50%, rgba(137, 211, 196, 1) 100%)"
+	},
+	{
+		accent_color: "#eb64ff",
+		accent_color_interaction: "#f08eff",
+		gradient: "linear-gradient(150deg, rgba(122, 8, 141, 1) 0%, rgba(203, 50, 172, 1) 50%, rgba(253, 81, 145, 1) 100%)"
+	},
+	{
+		accent_color: "#59d948",
+		accent_color_interaction: "#81e474",
+		gradient: "linear-gradient(150deg, rgba(44, 141, 8, 1) 0%, rgba(92, 203, 50, 1) 50%, rgba(139, 253, 81, 1) 100%)"
+	},
+	{
+		accent_color: "#ff9a8b",
+		accent_color_interaction: "#ff6a88",
+		gradient: "linear-gradient(150deg, rgba(255, 154, 139, 1) 0%, rgba(255, 106, 136, 1) 50%, rgba(255, 63, 63, 1) 100%)"
+	},
+	{
+		accent_color: "#8e2de2",
+		accent_color_interaction: "#4a00e0",
+		gradient: "linear-gradient(150deg, rgba(142, 45, 226, 1) 0%, rgba(74, 0, 224, 1) 50%, rgba(0, 0, 128, 1) 100%)"
+	},
+	{
+		accent_color: "#f7971e",
+		accent_color_interaction: "#ffd200",
+		gradient: "linear-gradient(150deg, rgba(247, 151, 30, 1) 0%, rgba(255, 210, 0, 1) 50%, rgba(255, 170, 0, 1) 100%)"
+	},
+	{
+		accent_color: "#00c6ff",
+		accent_color_interaction: "#0072ff",
+		gradient: "linear-gradient(150deg, rgba(0, 198, 255, 1) 0%, rgba(0, 114, 255, 1) 50%, rgba(0, 50, 255, 1) 100%)"
+	},
+	{
+		accent_color: "#76b852",
+		accent_color_interaction: "#8dc26f",
+		gradient: "linear-gradient(150deg, rgba(118, 184, 82, 1) 0%, rgba(141, 194, 111, 1) 50%, rgba(155, 210, 120, 1) 100%)"
+	},
+	{
+		accent_color: "#fc4a1a",
+		accent_color_interaction: "#f7b733",
+		gradient: "linear-gradient(150deg, rgba(252, 74, 26, 1) 0%, rgba(247, 183, 51, 1) 50%, rgba(251, 114, 76, 1) 100%)"
+	},
+	{
+		accent_color: "#11998e",
+		accent_color_interaction: "#38ef7d",
+		gradient: "linear-gradient(150deg, rgba(17, 153, 142, 1) 0%, rgba(56, 239, 125, 1) 50%, rgba(43, 212, 100, 1) 100%)"
+	},
+	{
+		accent_color: "#ee0979",
+		accent_color_interaction: "#ff6a00",
+		gradient: "linear-gradient(150deg, rgba(238, 9, 121, 1) 0%, rgba(255, 106, 0, 1) 50%, rgba(255, 40, 0, 1) 100%)"
+	},
+	{
+		accent_color: "#283c86",
+		accent_color_interaction: "#45a247",
+		gradient: "linear-gradient(150deg, rgba(40, 60, 134, 1) 0%, rgba(69, 162, 71, 1) 50%, rgba(56, 168, 50, 1) 100%)"
+	},
+	{
+		accent_color: "#52cc92",
+		accent_color_interaction: "#99f2c8",
+		gradient: "linear-gradient(150deg, rgba(31, 64, 55, 1) 0%, rgba(153, 242, 200, 1) 50%, rgba(0, 184, 148, 1) 100%)"
+	},
+	{
+		accent_color: "#c33764",
+		accent_color_interaction: "#1d2671",
+		gradient: "linear-gradient(150deg, rgba(195, 55, 100, 1) 0%, rgba(29, 38, 113, 1) 50%, rgba(0, 0, 50, 1) 100%)"
+	},
+	{
+		accent_color: "#ff7e5f",
+		accent_color_interaction: "#feb47b",
+		gradient: "linear-gradient(150deg, rgba(255, 126, 95, 1) 0%, rgba(254, 180, 123, 1) 50%, rgba(255, 140, 0, 1) 100%)"
+	},
+	{
+		accent_color: "#6a11cb",
+		accent_color_interaction: "#2575fc",
+		gradient: "linear-gradient(150deg, rgba(106, 17, 203, 1) 0%, rgba(37, 117, 252, 1) 50%, rgba(0, 64, 255, 1) 100%)"
+	},
+	{
+		accent_color: "#e53935",
+		accent_color_interaction: "#e35d5b",
+		gradient: "linear-gradient(150deg, rgba(229, 57, 53, 1) 0%, rgba(227, 93, 91, 1) 50%, rgba(255, 0, 0, 1) 100%)"
+	},
+	{
+		accent_color: "#56ab2f",
+		accent_color_interaction: "#a8e063",
+		gradient: "linear-gradient(150deg, rgba(86, 171, 47, 1) 0%, rgba(168, 224, 99, 1) 50%, rgba(124, 252, 0, 1) 100%)"
+	},
+	{
+		accent_color: "#e44d26",
+		accent_color_interaction: "#f16529",
+		gradient: "linear-gradient(150deg, rgba(228, 77, 38, 1) 0%, rgba(241, 101, 41, 1) 50%, rgba(255, 165, 0, 1) 100%)"
+	},
+	{
+		accent_color: "#34e89e",
+		accent_color_interaction: "#0f3443",
+		gradient: "linear-gradient(150deg, rgba(52, 232, 158, 1) 0%, rgba(15, 52, 67, 1) 50%, rgba(0, 128, 128, 1) 100%)"
+	}
+];
 
-// Function to take the random color and the color them and apply it to the chat GUI
+// Function to take the random color and the color theme and apply it to the chat GUI
 function setRandomTheme() {
 	const profilePic = document.querySelector(".profilePic");
-	profilePic.style.backgroundColor = getRandomColor();
-	const randomKey = Object.keys(colorThemes)[
-		Math.floor(Math.random() * Object.keys(colorThemes).length)
-	];
-	const theme = colorThemes[randomKey];
+	
+	const randomIndex = Math.floor(Math.random() * colorThemes.length);
+	const theme = colorThemes[randomIndex];
+	
+	profilePic.style.backgroundColor = theme.accent_color;
+	document.documentElement.style.setProperty("--accent-color", theme.accent_color);
+	document.documentElement.style.setProperty("--accent-color-interact", theme.accent_color_interaction);
+	document.body.style.background = theme.gradient;
 
-	document.documentElement.style.setProperty("--accent-color", theme[0]);
-	document.documentElement.style.setProperty(
-		"--accent-color-interact",
-		theme[1]
-	);
-	document.body.style.background = theme[2];
 	let incomingMessages = document.querySelectorAll(".inBubble");
 	incomingMessages.forEach((element) => {
-		const bgColor = window.getComputedStyle(element).backgroundColor;
-		setTextColorBasedOnBg(element, theme[1]);
+		setTextColorBasedOnBg(element, theme.accent_color);
 	});
 }
 
