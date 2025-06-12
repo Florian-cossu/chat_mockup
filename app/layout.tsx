d56c0951d7e6a13af06c41a8ee042065c53f35f6
Florian Cossu <florian.cossu@bodyguard.ai>
2025-06-13 00:50:27 +0200

    Manifest/Icon/favicon - Set new favicon, - Exported minimal icon sets ofr manifest, - Got screenshots for manifest, - Updated manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,12 @@ export default function RootLayout({
       <head>
         <meta name="theme-color" content="#ffffff" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="apple-touch-icon" href="/icons/icon-192x192.png" /> */}
+        <link rel="icon" href="/icons/icon-any.svg" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Your App Name" />
-        <meta name="description" content="Your app description goes here." />
+        <meta name="apple-mobile-web-app-title" content="Chat mockup" />
+        <meta name="description" content="Use this tool to generate custom chat screenshots to share and publish." />
         <meta name="mobile-web-app-capable" content="yes" />
       </head>
       <body
