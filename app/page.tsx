1593aba8fe358da7ea9c1682ce5f307eeb8dabc9
Florian Cossu <florian.cossu@bodyguard.ai>
2025-06-15 05:50:42 +0200

    Basics - Started adding basic visual props     - Message action bar,     - Icons     - Animations on hover

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,50 @@
 import Image from "next/image";
-import { Minus, ChevronsUpDown, Plus } from "lucide-react";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@components/ui/card";
+import { Minus, ChevronsUpDown, Plus, Camera, Paperclip, Send } from "lucide-react";
+import { Card, CardContent, CardFooter, CardHeader } from "@components/ui/card";
 
 export default function ChatMockup() {
   return (
     <div className="flex flex-column m-0 p-0 w-[100vw] h-[100vh] items-center justify-items-center justify-center">
       <Card className="w-4/5 m-0 p-0">
         <CardHeader className="flex flex-row m-0 p-0 h-fit items-center justify-end  border-b-1">
-          <div className="flex flex-row gap-1.5 p-2">
-            <span className="flex rounded-full w-4 h-4 bg-emerald-400 hover:bg-emerald-300 cursor-pointer transitions items-center justify-center">
-              <ChevronsUpDown className="w-3 h-3 rotate-45"/>
+          <div className="flex flex-row gap-1.5 p-2" id="windowButtons">
+            <span id="maximise" className="flex rounded-full w-4 h-4 bg-emerald-400 hover:bg-emerald-300 cursor-pointer transitions items-center justify-center">
+              <ChevronsUpDown className="w-3 h-3 rotate-45" />
             </span>
-            <span className="flex rounded-full w-4 h-4 bg-amber-400 hover:bg-amber-300 cursor-pointer transitions items-center justify-center">
-              <Minus className="w-3 h-3"/>
+            <span id="minimise" className="flex rounded-full w-4 h-4 bg-amber-400 hover:bg-amber-300 cursor-pointer transitions items-center justify-center">
+              <Minus className="w-3 h-3" />
             </span>
-            <span className="flex rounded-full w-4 h-4 bg-red-500 hover:bg-red-400 cursor-pointer transitions items-center justify-center">
-              <Plus className="w-3 h-3 rotate-45"/>
+            <span id="close" className="flex rounded-full w-4 h-4 bg-red-500 hover:bg-red-400 cursor-pointer transitions items-center justify-center">
+              <Plus className="w-3 h-3 rotate-45" />
             </span>
           </div>
         </CardHeader>
-        <CardContent>
-        <Image
-          className="dark:invert"
-          src="/icons/icon-any.svg"
-          alt="Icon"
-          width={180}
-          height={180}
-        />
+        <CardContent className="p-0 m-0">
+          <div className="flex flex-row gap-1.5 p-2" id="topActionBar">
+            <span className="rounded-full bg-transparent hover:bg-accent cursor-pointer">Test</span>
+          </div>
+          <Image
+            className="dark:invert"
+            src="/icons/icon-any.svg"
+            alt="Icon"
+            width={180}
+            height={180}
+          />
           <p>Card Content</p>
         </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
+        <CardFooter className="flex flex-row m-0 p-2 h-fit items-center justify-between border-t-1">
+          <input type="text" className="bg-accent rounded-full"></input>
+          <div id="messagingActions" className="flex flex-row gap-2">
+            <span id="photo" className="bg-transparent hover:bg-accent rounded-full p-1 cursor-pointer">
+              <Camera className="w-6 h-6" />
+            </span>
+            <span id="attachment" className="bg-transparent hover:bg-accent rounded-full p-1 cursor-pointer">
+              <Paperclip className="w-6 h-6" />
+            </span>
+            <span id="send" className="bg-sky-400 hover:bg-accent rounded-full p-1 cursor-pointer">
+              <Send className="w-6 h-6" />
+            </span>
+          </div>
         </CardFooter>
       </Card>
     </div>
